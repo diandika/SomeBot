c37adfd9bac5bf8d263eb771ac39e2c91c533fe7
Mohammad Dicky Andika Putra <mohammaddicky.andika@gmail.com>
2022-01-19 07:58:32 +0700

    response message

--- index.js
+++ index.js
@@ -56,11 +56,11 @@ function handleMessage(psid, message){
     let response;
 
     // Check if the message contains text
-    if (received_message.text) {    
+    if (message.text) {    
 
         // Create the payload for a basic text message
         response = {
-            "text": `You sent the message: "${received_message.text}". Now send me an image!`
+            "text": `You sent the message: "${message.text}". Now send me an image!`
         }
     }  
     
@@ -77,7 +77,7 @@ function callSendAPI(psid, res){
         "recipient": {
             "id": psid
         },
-        "message": response
+        "message": res
     }
 
     request({
